f3423450de619e45a79fbc6532ddc3dbb0699c1a
Dina Samatova <MY_NAME@example.com>
2025-02-27 12:19:25 +0100

    We put routes for Restaurant identity, and then we implemented CRUD methods and something more which you can find out if you look up this interesting project:)))))

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,19 +22,58 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
-
+  try {
+    const newRestaurant = Restaurant.build(req.body)
+    newRestaurant.userId = 1 // req.user.id
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
-
+  try {
+    await Restaurant.update(req.body, {
+      where: { id: req.params.restaurantId }
+    })
+    res.json(await Restaurant.findByPk(req.params.restaurantId))
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-
+  try {
+    await Restaurant.destroy({
+      where: { id: req.params.restaurantId }
+    })
+    res.json({ message: 'Restaurant deleted' })
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -2,6 +2,17 @@ import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
   // TODO: Include routes for restaurant described in the lab session README
-
+  app.route('/restaurants')
+    .get(
+      RestaurantController.index) //  FR1: List all restaurants
+    .post(
+      RestaurantController.create) // FR3: Create a restaurant
+  app.route('/restaurants/:restaurantId')
+    .put(
+      RestaurantController.update) // FR3: Update a restaurant
+    .delete(
+      RestaurantController.destroy) // FR3: Delete a restaurant
+    .get(
+      RestaurantController.show) // FR3: Show a restaurant
 }
 export default loadFileRoutes
